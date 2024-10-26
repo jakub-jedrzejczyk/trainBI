e72d0c0ccff5a4f4da02ed4f130423c64b2e8e66
Alght <107945278+Alght@users.noreply.github.com>
2024-10-26 12:15:56 +0200

    Create Drop_Database.sql

---
+++ database/Drop_Database.sql
@@ -0,0 +1,21 @@
+
+
+
+DROP TABLE Awaria;
+DROP TABLE Realizacja;
+
+DROP TABLE Odcinek;
+
+
+DROP TABLE Stacja;
+
+
+
+
+DROP TABLE Kurs;
+
+
+DROP TABLE Pociag;
+DROP TABLE Maszynista;
+
+ -- DROP DATABASE HD_lab_db;
