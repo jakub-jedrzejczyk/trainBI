60c9d4b71ad130ef69b0c36d9bd587cb85560136
Alght <107945278+Alght@users.noreply.github.com>
2024-10-28 16:18:37 +0100

    Update Insert.sql

--- database/Insert.sql
+++ database/Insert.sql
@@ -1,24 +1,48 @@
 USE HD_lab_db
 GO
 
+
+
+EXEC xp_fileexist 'C:\Users\annap\OneDrive\Dokumenty\SQL Server Management Studio\output';
+
+
 BULK INSERT dbo.Maszynista 
 FROM 'C:\Users\annap\Downloads\output\output\driver_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
-EXEC xp_fileexist 'C:\Users\annap\Downloads\output\output\driver_data.csv';
+BULK INSERT dbo.Pociag 
+FROM 'C:\Users\annap\Downloads\output\output\train_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
-DROP TABLE Awaria;
-DROP TABLE Realizacja;
-
-DROP TABLE Odcinek;
-
-
-DROP TABLE Stacja;
+BULK INSERT dbo.Stacja 
+FROM 'C:\Users\annap\Downloads\output\output\stop_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 
 
+BULK INSERT dbo.Odcinek 
+FROM 'C:\Users\annap\Downloads\output\output\section_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
-DROP TABLE Kurs;
 
 
-DROP TABLE Pociag;
+BULK INSERT dbo.Kurs 
+FROM 'C:\Users\annap\Downloads\output\output\relation_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
+
+
+
+
+BULK INSERT dbo.Realizacja 
+FROM 'C:\Users\annap\Downloads\output\output\realisation_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
+
+BULK INSERT dbo.Awaria 
+FROM 'C:\Users\annap\Downloads\output\output\fault_data.csv' 
+WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
+
+
+
+
+
+
