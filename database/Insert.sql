b606dc67f27b2548e987c107fea705324192e219
Alght <107945278+Alght@users.noreply.github.com>
2024-10-28 17:49:43 +0100

    insert modification

--- database/Insert.sql
+++ database/Insert.sql
@@ -2,43 +2,39 @@ USE HD_lab_db
 GO
 
 
-
-EXEC xp_fileexist 'path\output';
-
-
 BULK INSERT dbo.Maszynista 
-FROM 'path\driver_data.csv' 
+FROM 'path\output\1000000\driver_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 BULK INSERT dbo.Pociag 
-FROM 'path\train_data.csv' 
+FROM 'path\output\1000000\train_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 BULK INSERT dbo.Stacja 
-FROM 'path\stop_data.csv' 
+FROM 'path\output\1000000\stop_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 
 
 BULK INSERT dbo.Odcinek 
-FROM 'path\section_data.csv' 
+FROM 'path\output\1000000\section_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 
 
 BULK INSERT dbo.Kurs 
-FROM 'path\relation_data.csv' 
+FROM 'path\output\1000000\relation_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 
 
 
 BULK INSERT dbo.Realizacja 
-FROM 'path\realisation_data.csv' 
+FROM 'path\output\1000000\realisation_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 BULK INSERT dbo.Awaria 
-FROM 'path\fault_data.csv' 
+FROM 'path\output\1000000\fault_data.csv' 
 WITH (FIELDTERMINATOR=',', ROWTERMINATOR = '\n', FIRSTROW = 2)
 
 
