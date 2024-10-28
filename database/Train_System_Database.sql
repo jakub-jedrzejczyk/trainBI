51fa75d8b9d1562296e921d0e8e9aef20f874a8d
Alght <107945278+Alght@users.noreply.github.com>
2024-10-28 19:00:00 +0100

    Database modification

--- database/Train_System_Database.sql
+++ database/Train_System_Database.sql
@@ -11,32 +11,24 @@ CREATE TABLE Maszynista (
 	Nazwisko VARCHAR(20) NOT NULL
 );
 
-
---
 CREATE TABLE Stacja (
     ID_Stacja INT PRIMARY KEY,
-	
-	Nazwa VARCHAR(20) NOT NULL,
+	Nazwa VARCHAR(40) NOT NULL,
 
-	Miasto VARCHAR(20) NOT NULL,
-	Adres VARCHAR(40) NOT NULL,
+	Miasto VARCHAR(40) NOT NULL,
+	Adres VARCHAR(60) NOT NULL,
 	Liczba_Peronow INT NOT NULL,
 
 	Liczba_Torow INT NOT NULL
 );
 
-
-
 CREATE TABLE Pociag (
     ID_Pociag INT PRIMARY KEY,
-	Model INT NOT NULL, 
-	Ostatni_Przeglad INT NOT NULL,
+	Model VARCHAR(60) NOT NULL, 
+	Ostatni_Przeglad DATETIME NOT NULL,
 	Czy_Awaria BIT NOT NULL
 );
 
-
-
---
 CREATE TABLE Odcinek (
     ID_Odcinek INT PRIMARY KEY,
 	ID_Stacji_Start INT,
@@ -45,41 +37,21 @@ CREATE TABLE Odcinek (
     FOREIGN KEY (ID_Stacji_Koniec) REFERENCES Stacja(ID_Stacja)
 );
 
-
-
-
-
-
---
-
 CREATE TABLE Kurs (
     ID_Kurs CHAR(36) PRIMARY KEY,
-
 	PESEL_Maszynisty CHAR(11) NOT NULL,
 	ID_Pociag INT NOT NULL,
 	FOREIGN KEY (ID_Pociag) REFERENCES Pociag(ID_Pociag),
     FOREIGN KEY (PESEL_Maszynisty) REFERENCES Maszynista(PESEL)
 );
 
-
-
-
-
-
-
-
-
---
 CREATE TABLE Realizacja (
     ID_Realizacji INT PRIMARY KEY,
-	
 	ID_Kurs CHAR(36) NOT NULL,
-
 	ID_Odcinek INT,
 
 	Planowany_Odjazd DATETIME,
 	Planowany_Przyjazd DATETIME,
-
 	Rzeczywisty_Odjazd DATETIME,
 	Rzeczywisty_Przyjazd DATETIME,
 
@@ -89,7 +61,6 @@ CREATE TABLE Realizacja (
 );
 
 
---
 CREATE TABLE Awaria (
     ID_Awaria INT PRIMARY KEY,
 	Typ_Awarii VARCHAR(60) NOT NULL,
